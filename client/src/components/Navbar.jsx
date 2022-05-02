67767b1af1fad141e996941d8b3a8ff12372b555
Alex Silva <tatattamora@gmail.com>
2022-05-02 19:18:09 -0300

    test

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -200,7 +200,7 @@ const NavIcon = styled.div`
 
 // search style
 const SearchList = styled.div`
-    height: calc(100vh - 53px);
+    height: calc(100vh);
     padding-bottom: 53px;
     overflow-y: auto;
     width: 100%;

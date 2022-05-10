6d1729910726177152ba34fdbdd5aef3af35db6c
Alex Silva <tatattamora@gmail.com>
2022-05-09 23:50:39 -0300

    fix

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = (req,res,next) => {
     if(authHeader){
         const token = authHeader.split(" ")[1];
 
-        jwt.verify(JSON.stringify(token), process.env.JWT_SEC, (err,user)=> {
+        jwt.verify(token, process.env.JWT_SEC, (err,user)=> {
             if(err) res.status(403).json("Token is not valid");
 
             req.user = user;
